73299b75962e1ceb7b2d7a6a7ef7e5bef1e919c0
Sevmamra <sevmamra01@gmail.com>
2025-05-23 22:26:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-    TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-    AUTHORIZED_USER_ID = int(os.getenv("AUTHORIZED_USER_ID"))  # Your Telegram user ID
-    ADMIN_GROUP_ID = int(os.getenv("ADMIN_GROUP_ID", -1001234567890))  # Your admin group ID
+    TOKEN = os.getenv("7763888885:AAGGtCSMRc0tvvGCFJwRMmYJBR819JfiOmQ")
+    AUTHORIZED_USER_ID = int(os.getenv("6567162029"))  # Your Telegram user ID
+    ADMIN_GROUP_ID = int(os.getenv("ADMIN_GROUP_ID", -1002501498159))  # Your admin group ID
